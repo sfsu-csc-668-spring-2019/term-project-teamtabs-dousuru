a896641a819832019b0b01819f794bff1e2d3844
Eric Groom <egroom@mail.sfsu.edu>
2019-04-15 16:27:06 -0700

    basic dashboard layout

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { OrganizationListComponent } from "./organization-list/organization-list.component";
+import { ProjectsListComponent } from "./projects-list/projects-list.component";
+import { ProjectDetailComponent } from "./project-detail/project-detail.component";
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    OrganizationListComponent,
+    ProjectsListComponent,
+    ProjectDetailComponent
+  ],
   imports: [CommonModule, DashboardRoutingModule],
   exports: [DashboardComponent]
 })
