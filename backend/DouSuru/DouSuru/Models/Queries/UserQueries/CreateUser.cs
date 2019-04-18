99000422a773236271a2b4bde6fbb325382305de
Michael <mwinata@mail.sfsu.edu>
2019-04-18 00:17:55 -0700

    add create user query

---
+++ backend/DouSuru/DouSuru/Models/Queries/UserQueries/CreateUser.cs
@@ -0,0 +1,19 @@
+﻿using DouSuru.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DouSuru.Models.Queries.UserQueries
+{
+    public class CreateUser : Query
+    {
+        public CreateUser()
+        {
+            QueryString = "INSERT INTO users VALUES ('@user_name', '@password', '@display_name', '@user_icon', '@user_email')";
+        }
+
+        public override JsonResult Execute( DouSuruContext context, JsonResult parameters )
+        {
+            return new JsonResult( new { data = context.Database.ExecuteSqlCommand( QueryString, parameters ) } );
+        }
+    }
+}
