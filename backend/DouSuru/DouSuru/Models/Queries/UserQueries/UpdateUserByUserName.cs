73bb476787ed61d2d8f9f2829e30454af2f4c29d
Michael <mwinata@mail.sfsu.edu>
2019-04-18 00:17:12 -0700

    add quotation marks to follow sql syntax

--- backend/DouSuru/DouSuru/Models/Queries/UserQueries/UpdateUserByUserName.cs
+++ backend/DouSuru/DouSuru/Models/Queries/UserQueries/UpdateUserByUserName.cs
@@ -2,5 +2,5 @@
 
 namespace DouSuru.Models.Queries.UserQueries
 {
-    public class UpdateUserByUserName : Query     {         public UpdateUserByUserName()         {             QueryString = "UPDATE users SET (display_name, user_icon, user_email) =(@display_name, @user_icon, @user_email) WHERE user_name = @user_name";         }          public override JsonResult Execute( DouSuruContext context, JsonResult parameters )         {             return new JsonResult( new { data = context.User.FromSql( QueryString, parameters ) } );         }     }
+    public class UpdateUserByUserName : Query     {         public UpdateUserByUserName()         {             QueryString = "UPDATE users SET (display_name, user_icon, user_email) = ('@display_name', '@user_icon', '@user_email') WHERE user_name = '@user_name'";         }          public override JsonResult Execute( DouSuruContext context, JsonResult parameters )         {             return new JsonResult( new { data = context.User.FromSql( QueryString, parameters ) } );         }     }
 }
