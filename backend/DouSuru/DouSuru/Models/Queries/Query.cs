e19fe023ec866f81e4c9c5d340dbf696a9eb9152
Michael <mwinata@mail.sfsu.edu>
2019-04-17 21:00:06 -0700

    refactor naming convention

--- backend/DouSuru/DouSuru/Models/Queries/Query.cs
+++ backend/DouSuru/DouSuru/Models/Queries/Query.cs
@@ -5,7 +5,7 @@ namespace DouSuru.Data.Queries
 {
     public abstract class Query
     {
-        public string query_string;
-        public abstract JsonResult execute(DouSuruContext context, object[] parameters); //returns a json string or null if write query
+        public string QueryString;
+        public abstract JsonResult Execute(DouSuruContext context, object[] parameters); //returns a json string or null if write query
     }
 }
