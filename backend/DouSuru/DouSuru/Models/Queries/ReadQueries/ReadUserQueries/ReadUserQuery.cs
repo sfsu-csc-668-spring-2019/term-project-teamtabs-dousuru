4c568a1002f73983ee5177b9ce8eef5969d867c4
Michael <mwinata@mail.sfsu.edu>
2019-04-17 21:28:01 -0700

    formatting: remove extra space

--- backend/DouSuru/DouSuru/Models/Queries/ReadQueries/ReadUserQueries/ReadUserQuery.cs
+++ backend/DouSuru/DouSuru/Models/Queries/ReadQueries/ReadUserQueries/ReadUserQuery.cs
@@ -6,7 +6,6 @@ namespace DouSuru.Data.Queries.ReadQueries.ReadUserQueries
 {
     public class ReadUserQuery : Query
     {
-
         public override JsonResult Execute( DouSuruContext context, object[] parameters )
         {
             return new JsonResult(new { data = context.User.FromSql( QueryString, parameters ) });
--- backend/DouSuru/DouSuru/Models/Queries/WriteQueries/WriteQuery.cs
+++ backend/DouSuru/DouSuru/Models/Queries/WriteQueries/WriteQuery.cs
@@ -6,7 +6,6 @@ namespace DouSuru.Data.Queries.WriteQueries
 {
     public class WriteQuery : Query
     {
-
         public override JsonResult Execute( DouSuruContext context, object[] parameters )
         {
             return new JsonResult( new { data = context.Database.ExecuteSqlCommand( QueryString, parameters ) } );
