d83c47e7d2988008b657d65da73ff5582df2305a
Michael <mwinata@mail.sfsu.edu>
2019-04-17 21:24:41 -0700

    defined ReadUserQuery structure

---
+++ backend/DouSuru/DouSuru/Models/Queries/ReadQueries/ReadUserQuery.cs
@@ -0,0 +1,15 @@
+﻿using DouSuru.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DouSuru.Data.Queries.ReadQueries
+{
+    public class ReadUserQuery : Query
+    {
+
+        public override JsonResult Execute( DouSuruContext context, object[] parameters )
+        {
+            return new JsonResult(new { data = context.User.FromSql( QueryString, parameters ) });
+        }
+    }
+}
