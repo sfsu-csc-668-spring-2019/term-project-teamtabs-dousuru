d75bb2bacce44664aa81f3d3beb7f8a313a40f72
Michael <mwinata@mail.sfsu.edu>
2019-04-17 23:36:11 -0700

    add update user by user name query

---
+++ backend/DouSuru/DouSuru/Models/Queries/UserQueries/UpdateUserByUserName.cs
@@ -0,0 +1,6 @@
+﻿using DouSuru.DAL; using Microsoft.AspNetCore.Mvc; using Microsoft.EntityFrameworkCore;
+
+namespace DouSuru.Models.Queries.UserQueries
+{
+    public class UpdateUserByUserName : Query     {         public UpdateUserByUserName()         {             QueryString = "UPDATE users SET (display_name, user_icon, user_email) =(@display_name, @user_icon, @user_email) WHERE user_name = @user_name";         }          public override JsonResult Execute( DouSuruContext context, JsonResult parameters )         {             return new JsonResult( new { data = context.User.FromSql( QueryString, parameters ) } );         }     }
+}
