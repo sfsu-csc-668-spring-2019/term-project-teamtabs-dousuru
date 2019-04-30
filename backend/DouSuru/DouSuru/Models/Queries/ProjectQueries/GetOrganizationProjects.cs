aa29e45c550658467a3c71918205e2e479a36a4a
Eric Groom <egroom@mail.sfsu.edu>
2019-04-29 20:22:28 -0700

    remove asp

--- backend/DouSuru/DouSuru/Models/Queries/ProjectQueries/GetOrganizationProjects.cs
+++
@@ -1,22 +0,0 @@
-﻿using DouSuru.DAL;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Linq;
-using Npgsql;
-using System;
-using System.Data.SqlClient;
-
-namespace DouSuru.Models.Queries.ProjectQueries
-{
-    public class GetOrganizationProjects:Query
-    {
-        public GetOrganizationProjects()         {             QueryString = "SELECT * FROM projects WHERE project_id IN " +
-                "( SELECT project_id FROM organization_projects WHERE organization_id = @organization_id ) ; ";         }          public override JsonResult Execute(DouSuruContext context, JObject parameters)
-        {
-            context.Organization.FromSql(
-                QueryString,
-                new NpgsqlParameter("organization_id", (string)parameters["organization_id"]));
-            return null;
-        }
-    }
-}
--- backend/DouSuru/DouSuru/Models/Queries/UserQueries/GetUserInformationByUserName.cs
+++
@@ -1,5 +0,0 @@
-﻿using DouSuru.DAL; using Microsoft.AspNetCore.Mvc; using Microsoft.EntityFrameworkCore; using Newtonsoft.Json.Linq;
-using Npgsql;
-using System.Data.SqlClient;
-
-namespace DouSuru.Models.Queries.UserQueries {     public class GetUserInformationByUserName : Query     {         public GetUserInformationByUserName()         {             QueryString = "SELECT user_id, user_name, password, display_name, icon, email FROM users WHERE user_name = @user_name;";         }          public override JsonResult Execute( DouSuruContext context, JObject parameters )         {             return new JsonResult( context.User.FromSql( QueryString, new NpgsqlParameter( "user_name", ( string ) parameters[ "user_name" ] ) ) );         }     } } 
