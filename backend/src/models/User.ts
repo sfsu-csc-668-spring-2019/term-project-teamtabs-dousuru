99e7065e5a939040273a879d2c2bbcc503abb06c
Richard Robinson <rrobins1@mail.sfsu.edu>
2019-04-29 21:39:35 -0700

    user model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,13 +1,25 @@
-import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn, OneToMany, JoinTable, UpdateDateColumn } from "typeorm";
+import { Organization } from "./Organization";
+import { JoinAttribute } from "typeorm/query-builder/JoinAttribute";
 
 @Entity()
 export class User extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn("increment")
+  userID: number;
 
-  @Column()
+  @Column({ type: "varchar", length: 40, unique: true, nullable: false})
+  userName: string
+
+  @Column({ type: "varchar", length: 100, unique: true, nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", unique: true, nullable: false})
+  displayName: string;
+
+
+  @Column({ type: "varchar", length: 200, unique: true })
   email: string;
 
-  @Column()
-  password: string;
+  @Column({ type: "varchar", length: 2083 })
+  icon: string;
 }
