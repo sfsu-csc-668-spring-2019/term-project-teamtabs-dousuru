1dd226d45bd7efa5403c70d2b11dd018f05c1943
Peter Le <peterle408@gmail.com>
2019-04-29 22:03:09 -0700

    project and user relation model

--- backend/src/models/Project.ts
+++ backend/src/models/Project.ts
@@ -1,20 +1,24 @@
-import { Entity, BaseEntity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { Entity, BaseEntity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from "typeorm";
 import { User } from "./User";
 
 @Entity()
 export class Project extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn("increment")
   id: number;
 
-  @Column()
+  @Column({ type: "varchar", length: 60, nullable: false })
   name: string;
 
-  @Column()
+  @Column({ type: "varchar", length: 5000 })
   description: string;
 
-  @Column()
+  @Column({ type: "boolean", nullable: false })
   isPublic: boolean;
 
-  @ManyToOne(type => User, user => user.projects)
-  user: User;
+  @ManyToOne(type => User, user => user.ownedProjects)
+  owner: User;
+
+  @ManyToMany( type => User)
+  @JoinTable()
+  users: User[];
 }
