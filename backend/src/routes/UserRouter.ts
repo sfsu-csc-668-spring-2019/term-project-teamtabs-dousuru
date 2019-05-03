89c2f71ffd283ab8a511c7e95b6f6dab3eb18a07
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-03 16:32:12 -0700

    [refactor] took out inline functions and gave them names

--- backend/src/routes/UserRouter.ts
+++ backend/src/routes/UserRouter.ts
@@ -8,33 +8,35 @@ import { AuthRequest } from "../types/AuthRequest";
 export class UserRouter extends Router {
   protected setServices(): void {
     this.services = new Map();
-    this.services.set("PUT /signup", async (req: any, res: any) => {
-      const { email, password, displayName, userName, icon } = req.body;
+    this.services.set("PUT /signup", this.signUp);
+    this.services.set("GET /auth_valid", this.authValid);
+  }
 
-      if (!email || !password || !displayName || !userName) {
-        return res.status(400);
-      }
-      const encryptedPassword = await SecretsService.encrypt(password);
+  private async signUp(req: Request, res: Response): Promise<void> {
+    const { email, password, displayName, userName, icon } = req.body;
+    if (!email || !password || !displayName || !userName) {
+      res.status(400);
+      return;
+    }
+    const encryptedPassword = await SecretsService.encrypt(password);
+    try {
+      const { id: userId } = await UserManager.createAccount(
+        email,
+        encryptedPassword,
+        displayName,
+        userName,
+        icon
+      );
+      res.json({ token: SecretsService.createToken(userId) });
+    } catch (err) {
+      console.log(err);
+      res.status(500);
+    }
+  }
 
-      try {
-        const { id: userId } = await UserManager.createAccount(
-          email,
-          encryptedPassword,
-          displayName,
-          userName,
-          icon
-        );
-        res.json({ token: SecretsService.createToken(userId) });
-      } catch (err) {
-        console.log(err);
-        res.status(500);
-      }
-    });
-    this.services.set("GET /auth_valid", (req: Request, res: Response) => {
-      authenticate(req, res, (req: AuthRequest, res: Response) => {
-        const user = req.user;
-        res.send(!!user);
-      });
+  private authValid(req: Request, res: Response): void {
+    authenticate(req, res, (req: AuthRequest, res: Response) => {
+      res.send(!!req.user);
     });
   }
 }
