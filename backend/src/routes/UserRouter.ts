a150f504920d0df0a1271217d6999d0364175880
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-03 16:54:06 -0700

    declared all user routes

--- backend/src/routes/UserRouter.ts
+++ backend/src/routes/UserRouter.ts
@@ -8,11 +8,90 @@ import { AuthRequest } from "../types/AuthRequest";
 export class UserRouter extends Router {
   protected setServices(): void {
     this.services = new Map();
-    this.services.set("PUT /signup", this.signUp);
-    this.services.set("GET /auth_valid", this.authValid);
+    this.services.set("GET /auth_valid", this.getAuthValid);
+    this.services.set("GET /search", this.getUserSearch);
+    this.services.set("GET /id/:userId", this.getUserPage);
+    this.services.set(
+      "GET /id/:userId/configuration",
+      this.getUserConfiguration
+    );
+    this.services.set("GET /id/:userId/notification", this.getUserNotification);
+    this.services.set(
+      "GET /id/:userId/chatlog/id/:chatId",
+      this.getUserChatlog
+    );
+    this.services.set("POST /search", this.postUserSearch);
+    this.services.set(
+      "POST /id/:userId/configuration",
+      this.postUserConfiguration
+    );
+    this.services.set(
+      "POST /id/:userId/notification",
+      this.postUserNotification
+    );
+    this.services.set(
+      "POST /id/:userId/chatlog/id/:chatId",
+      this.postUserChatlog
+    );
+    this.services.set(
+      "POST /id/:userId/chatlog/id/:chatId/search",
+      this.postUserChatlogSearch
+    );
+    this.services.set("POST /id/:userId/search", this.postUserSearchContents);
+    this.services.set("PUT /signup", this.putUserSignup);
   }
 
-  private async signUp(req: Request, res: Response): Promise<void> {
+  private getAuthValid(req: Request, res: Response): void {
+    authenticate(req, res, (req: AuthRequest, res: Response) => {
+      res.send(!!req.user);
+    });
+  }
+
+  private getUserSearch(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private getUserPage(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private getUserConfiguration(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private getUserNotification(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private getUserChatlog(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserSearch(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserConfiguration(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserNotification(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserChatlog(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserChatlogSearch(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private postUserSearchContents(_: Request, res: Response): void {
+    res.status(404);
+  }
+
+  private async putUserSignup(req: Request, res: Response): Promise<void> {
     const { email, password, displayName, userName, icon } = req.body;
     if (!email || !password || !displayName || !userName) {
       res.status(400);
@@ -32,11 +111,5 @@ export class UserRouter extends Router {
       console.log(err);
       res.status(500);
     }
-  }
-
-  private authValid(req: Request, res: Response): void {
-    authenticate(req, res, (req: AuthRequest, res: Response) => {
-      res.send(!!req.user);
-    });
   }
 }
