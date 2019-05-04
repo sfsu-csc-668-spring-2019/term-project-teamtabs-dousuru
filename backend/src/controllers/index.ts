d7ed35c2bb27e64880a6b6246523b5a85015ae91
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-03 19:24:20 -0700

    init controller index and managers

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from "./ListManager";
+export * from "./OrganizationManager";
+export * from "./ProjectManager";
+export * from "./RoleManager";
+export * from "./TagManager";
+export * from "./TaskManager";
+export * from "./UserManager";
