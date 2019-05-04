0d24d242a563824986ea7540681492dbe7bea4bb
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-03 19:50:01 -0700

    finished TagManager

--- backend/src/controllers/TagManager.ts
+++ backend/src/controllers/TagManager.ts
@@ -1,1 +1,28 @@
-export class TagManager {}
+import { Tag } from "../entity";
+
+export class TagManager {
+  public static async createTag(name: string, color: string): Promise<Tag> {
+    let tag = await Tag.create({ name, color });
+    return await tag.save();
+  }
+
+  public static async removeTag(tagId: number): Promise<void> {
+    let tag = await Tag.findOne(tagId);
+    await Tag.remove(tag);
+  }
+
+  public static async updateTag(
+    tagId: number,
+    name: string,
+    color: string
+  ): Promise<Tag> {
+    const tag = await Tag.findOne(tagId);
+    tag.name = name;
+    tag.color = color;
+    return await tag.save();
+  }
+
+  public static async getTag(tagId: number): Promise<Tag> {
+    return await Tag.findOne(tagId);
+  }
+}
