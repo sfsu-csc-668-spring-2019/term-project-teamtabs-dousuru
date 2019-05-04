18ee9562e3984b9b8d4d9e46643c8349f8826b95
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-03 21:12:32 -0700

    using querybuilder to access the data in the database

--- backend/src/controllers/TagManager.ts
+++ backend/src/controllers/TagManager.ts
@@ -1,28 +1,56 @@
-import { Tag } from "../entity";
+import { Tag, Task, Project } from "../entity";
+import { getConnection } from "typeorm";
 
 export class TagManager {
-  public static async createTag(name: string, color: string): Promise<Tag> {
-    let tag = await Tag.create({ name, color });
+  public static async createTag(
+    name: string,
+    color: string,
+    projectId: number
+  ): Promise<Tag> {
+    let basedProject = await getConnection()
+      .createQueryBuilder()
+      .select("project")
+      .from(Project, "project")
+      .where("project.id = :projectId", { projectId })
+      .getOne();
+    let tag = await Tag.create({ name, color, basedProject });
     return await tag.save();
   }
 
   public static async removeTag(tagId: number): Promise<void> {
-    let tag = await Tag.findOne(tagId);
-    await Tag.remove(tag);
+    await getConnection()
+      .createQueryBuilder()
+      .delete()
+      .from(Tag)
+      .where("id = :tagId", { tagId })
+      .execute();
   }
 
   public static async updateTag(
     tagId: number,
     name: string,
     color: string
-  ): Promise<Tag> {
-    const tag = await Tag.findOne(tagId);
-    tag.name = name;
-    tag.color = color;
-    return await tag.save();
+  ): Promise<JSON> {
+    await getConnection()
+      .createQueryBuilder()
+      .update(Tag)
+      .set({ name, color })
+      .where("id = :tagId", { tagId })
+      .execute();
+    return await this.getTag(tagId);
   }
 
-  public static async getTag(tagId: number): Promise<Tag> {
-    return await Tag.findOne(tagId);
+  public static async getTasks(tagId: number): Promise<Task[]> {
+    let tag = await Tag.findOne(tagId);
+    return tag.tasks;
+  }
+
+  public static async getTag(tagId: number): Promise<JSON> {
+    return await getConnection()
+      .createQueryBuilder()
+      .select(["tag.id", "tag.name", "tag.color", "tag.basedProject"])
+      .from(Tag, "tag")
+      .where("tag.id = :tagId", { tagId })
+      .getRawOne();
   }
 }
