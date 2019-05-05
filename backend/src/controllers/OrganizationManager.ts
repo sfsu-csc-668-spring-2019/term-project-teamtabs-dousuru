371a3eea204697800a7d0833574cae1296f7103d
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-05 15:23:04 -0700

    implement organization manager

--- backend/src/controllers/OrganizationManager.ts
+++ backend/src/controllers/OrganizationManager.ts
@@ -1,25 +1,85 @@
-import { Organization, User, Project } from "../entity";
+import { Organization, User, Project, Message } from "../entity";
+import { SecretsService } from "./SecretsService";
 
 export class OrganizationManager {
-  static async createOrganization(name: string, description: string, icon: string, ownerID: number): Promise<Organization> {
-    const owner = await User.findOne(ownerID);
-    const organization = await Organization.create({
+  public static async createOrganization(
+    name: string,
+    description: string,
+    icon: string,
+    ownerId: number
+  ): Promise<Organization> {
+    let owner = await User.findOne(ownerId);
+    let inviteLink = SecretsService.createToken(await Organization.count());
+    let organization = await Organization.create({
       name,
       description,
+      inviteLink,
       icon,
       owner
     });
-    return organization.save();
+    return await organization.save();
   }
 
-  //get users for organization
-  static async getOrganizationUsers(organizationID: number): Promise<User[]> {
-    const organization = await Organization.findOne(organizationID);
+  public static async updateOrganization(
+    organizationId: number,
+    name: string,
+    description: string,
+    icon: string
+  ): Promise<Organization> {
+    let organization = await Organization.findOne(organizationId);
+    organization.name = name;
+    organization.description = description;
+    organization.icon = icon;
+    return await organization.save();
+  }
+
+  public static async deleteOrganization(
+    organizationId: number
+  ): Promise<void> {
+    await Organization.delete(organizationId);
+  }
+
+  public static async getOrganization(
+    organizationId: number
+  ): Promise<Organization> {
+    return await Organization.findOne(organizationId);
+  }
+
+  public static async getOrganizationUsers(
+    organizationId: number
+  ): Promise<User[]> {
+    let organization = await Organization.findOne(organizationId);
     return organization.users;
   }
 
-  static async getOrganizationProjects(organizationID: number): Promise<Project[]>{
-      const organization = await Organization.findOne(organizationID);
-      return organization.containedProjects;
+  public static async getOrganizationProjects(
+    organizationId: number
+  ): Promise<Project[]> {
+    let organization = await Organization.findOne(organizationId);
+    return organization.containedProjects;
   }
-}   
+  public static async changeOwner(
+    organizationId: number,
+    ownerId: number
+  ): Promise<Organization> {
+    let owner = await User.findOne(ownerId);
+    let organization = await Organization.findOne(organizationId);
+    organization.owner = owner;
+    return await organization.save();
+  }
+  public static async getInviteLink(organizationId: number): Promise<string> {
+    return (await Organization.findOne(organizationId)).inviteLink;
+  }
+  public static async getMessage(organizationId: number): Promise<Message[]> {
+    return (await Organization.findOne(organizationId)).organizationMessages;
+  }
+  public static async postMessage(
+    organizationId: number,
+    messageId: number
+  ): Promise<void> {
+    let message = await Message.findOne(messageId);
+    let organization = await Organization.findOne(organizationId);
+    organization.organizationMessages.push(message);
+    await organization.save();
+  }
+}
