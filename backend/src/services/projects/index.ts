82a35f036af589c77fd9499b160cf63fd411c1b5
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-09 16:58:53 -0700

    removed two services with no usage
    GetProjectCreate -> doesn't do anything
    PostProjectChatlogSearch -> filtration can be done on user end to reduce server load

--- backend/src/services/projects/index.ts
+++ backend/src/services/projects/index.ts
@@ -1,10 +1,8 @@
 import { GetProjectChatlogData } from "./GetProjectChatlogData";
 import { GetProjectConfiguration } from "./GetProjectConfiguration";
-import { GetProjectCreate } from "./GetProjectCreate";
 import { GetProjectData } from "./GetProjectData";
 import { GetProjectNotification } from "./GetProjectNotification";
 import { PostProjectChatlog } from "./PostProjectChatlog";
-import { PostProjectChatlogSearch } from "./PostProjectChatlogSearch";
 import { PostProjectConfiguration } from "./PostProjectConfiguration";
 import { PostProjectCreate } from "./PostProjectCreate";
 import { PostProjectDelete } from "./PostProjectDelete";
@@ -14,11 +12,9 @@ import { PostProjectSearch } from "./PostProjectSearch";
 export default [
   GetProjectChatlogData,
   GetProjectConfiguration,
-  GetProjectCreate,
   GetProjectData,
   GetProjectNotification,
   PostProjectChatlog,
-  PostProjectChatlogSearch,
   PostProjectConfiguration,
   PostProjectCreate,
   PostProjectDelete,
