7cee5b7e391d207ef4e5b56ff89bdf41f92f2696
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-08 23:56:11 -0700

    defined PostOrganizationLeave

--- backend/src/services/organizations/PostOrganizationLeave.ts
+++ backend/src/services/organizations/PostOrganizationLeave.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { IService, IMiddlewareFunction } from "..";
+import { OrganizationManager } from "../../controllers";
+import authenticate from "../../middleware/authMiddleware";
+import { AuthRequest } from "../../types/AuthRequest";
+import { Organization } from "../../entity";
 
 export class PostOrganizationLeave implements IService {
   public getRoute(): string {
@@ -7,8 +11,34 @@ export class PostOrganizationLeave implements IService {
   }
 
   public execute(): IMiddlewareFunction {
-    return (_: Request, response: Response, __: NextFunction) => {
-      response.sendStatus(404);
+    return (request: Request, response: Response, __: NextFunction) => {
+      const {
+        params: { organizationId }
+      } = request;
+      authenticate(
+        request,
+        response,
+        (request: AuthRequest, response: Response) =>
+          this.validate(organizationId, request)
+            .then(_ => response.sendStatus(200))
+            .catch(_ => response.sendStatus(500))
+      );
     };
+  }
+
+  public validate(
+    organizationId: number,
+    request: AuthRequest
+  ): Promise<Organization> {
+    if (request.user) {
+      return Promise.resolve(
+        OrganizationManager.removeOrganizationUser(
+          organizationId,
+          request.user.id
+        )
+      );
+    } else {
+      return Promise.reject();
+    }
   }
 }
