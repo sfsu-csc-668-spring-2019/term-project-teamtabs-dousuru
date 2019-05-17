04f4d5d1e0276a7876e7ed06f2a77b5e4696fe82
Eric Groom <egroom@mail.sfsu.edu>
2019-05-17 16:26:53 -0700

    update list organizations route to extend auth service

--- backend/src/services/organizations/ListOrganizations.ts
+++ backend/src/services/organizations/ListOrganizations.ts
@@ -1,29 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import { IService, IMiddlewareFunction } from "..";
-import { Organization, User } from "../../entity";
-import authenticate from "../../middleware/authMiddleware";
+import { IMiddlewareFunction, AuthenticatedService } from "..";
+import { User } from "../../entity";
 import { AuthRequest } from "../../types/AuthRequest";
 import { UserManager } from "../../controllers/UserManager";
 
-export class ListOrganizations implements IService {
+export class ListOrganizations extends AuthenticatedService {
   public getRoute(): string {
     return "GET /list/";
   }
 
-  public execute(): IMiddlewareFunction {
+  public authenticatedExecute(): IMiddlewareFunction {
     return (request: Request, response: Response, _: NextFunction) => {
       console.log("got list");
-      authenticate(
-        request,
-        response,
-        (request: AuthRequest, response: Response) =>
-          this.validate(request)
-            .then(user => UserManager.getOrganizations(user.id))
-            .then(orgs => response.json(orgs))
-            .catch(_ => {
-              response.sendStatus(400);
-            })
-      );
+      this.validate(request)
+        .then(user => UserManager.getOrganizations(user.id))
+        .then(orgs => response.json(orgs))
+        .catch(_ => {
+          response.sendStatus(400);
+        });
     };
   }
 
