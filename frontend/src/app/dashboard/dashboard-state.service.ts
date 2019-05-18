6f5ce87bd31c1685afe2e2406d617337d705c6f8
swabisan <michaelrogerswanson@gmail.com>
2019-05-17 17:02:58 -0700

    dashboard state-service

--- frontend/src/app/dashboard/dashboard-state.service.ts
+++ frontend/src/app/dashboard/dashboard-state.service.ts
@@ -1,9 +1,17 @@
+import { ApiService } from "./../networking/api.service";
 import { Injectable } from "@angular/core";
 import { Organization } from "../models";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class DashboardStateService {
-  constructor() {}
+  selectedOrganization: Organization;
+
+  constructor(private apiService: ApiService) {}
+
+  getOrganizations(): Observable<Organization[]> {
+    return this.apiService.getOrganizations();
+  }
 }
--- frontend/src/app/dashboard/organization-list/organization-list.component.ts
+++ frontend/src/app/dashboard/organization-list/organization-list.component.ts
@@ -2,6 +2,7 @@ import { DashboardStateService } from "./../dashboard-state.service";
 import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { Organization } from "../../models";
+import { ApiService } from "src/app/networking/api.service";
 
 @Component({
   selector: "app-organization-list",
@@ -12,7 +13,9 @@ export class OrganizationListComponent implements OnInit {
   organizations: Observable<Organization[]>;
   constructor(private dashboardStateService: DashboardStateService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.organizations = this.dashboardStateService.getOrganizations();
+  }
 
   selectOrganization(id: number) {
     console.log("selectOrganization");
