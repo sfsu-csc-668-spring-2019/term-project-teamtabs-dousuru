37dd5c76d5473fdc5dbb475ae73508c3682c8840
Peter Le <peterle408@gmail.com>
2019-05-18 21:20:31 -0700

    [WIP] landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { HomepageComponent } from "./homepage/homepage.component";
 import { TodoComponent } from "./todo/todo/todo.component";
 import { AuthGuard } from "./auth/auth.guard";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 const routes: Routes = [
   { path: "", component: HomepageComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "todo", component: TodoComponent }
+  { path: "todo", component: TodoComponent },
+  { path: "landing", component: LandingPageComponent }
 ];
 
 @NgModule({
