2ba5c88e9e952c85e2a4ac18351954e50ee2e19d
Eric Groom <egroom@mail.sfsu.edu>
2019-05-20 12:18:55 -0700

    working image upload

--- frontend/src/app/dashboard/edit-organization/edit-organization.component.ts
+++ frontend/src/app/dashboard/edit-organization/edit-organization.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { HttpClient, HttpBackend } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { FormBuilder } from "@angular/forms";
-import { Observable, of } from "rxjs";
+import { Observable } from "rxjs";
 import { Cloudinary } from "@cloudinary/angular-5.x";
 import { Organization } from "../../models";
 import { DashboardStateService } from "../dashboard-state.service";
@@ -16,54 +16,64 @@ export class EditOrganizationComponent implements OnInit {
     private state: DashboardStateService,
     private fb: FormBuilder,
     private cloudinary: Cloudinary,
-    private http: HttpBackend
+    private http: HttpClient
   ) {}
 
   @Input() organization: Organization;
   // possible race condition
   inviteLink: Observable<string>;
+  imageLink: string;
+  uploading = false;
 
   formGroup = this.fb.group({
     name: [""],
-    description: [""],
-    icon: [""]
+    description: [""]
   });
 
   ngOnInit() {
     this.inviteLink = this.state.getInviteLink(this.organization);
   }
 
-  save() {
-    const org = this.organization;
-    if (!org || !this.formGroup.valid) {
-      return;
-    }
-    const { name, description, icon } = this.formGroup.value;
-    this.upload(icon)
-      .pipe()
+  changeFile(event) {
+    this.uploading = true;
+    const file = event.target.files[0];
+    this.upload(file)
       .toPromise()
       .then(value => {
-        console.log(value);
+        const url = value.secure_url;
+        this.imageLink = url;
+      })
+      .finally(() => {
+        this.uploading = false;
       });
+  }
+
+  save() {
+    const org = this.organization;
+    if (!org || !this.formGroup.valid || this.uploading) {
+      return;
+    }
+    const { name, description } = this.formGroup.value;
     org.name = name;
     org.description = description;
-    org.icon = icon;
+    org.icon = this.imageLink;
     this.state
       .updateOrganization(org)
       .toPromise()
       .then(updatedOrg => {
         console.log(updatedOrg);
+      })
+      .catch(err => {
+        console.error(err);
       });
   }
 
-  upload(icon: any): any {
+  upload(icon: any): Observable<any> {
     const cloudName = this.cloudinary.cloudinaryInstance.config().cloud_name;
     const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload?upload_preset=dousuru`;
     console.log(url);
     const formData = new FormData();
     formData.append("file", icon);
-    return fetch(url, { method: "POST", body: formData }).then(stuff =>
-      console.log(stuff)
-    );
+    return this.http.post(url, formData);
   }
 }
