56e0784483752b3ba08ed4087e3aa5446599cae2
Richard Robinson <rrobins1@mail.sfsu.edu>
2019-05-19 18:37:52 -0700

    Updating task should work

--- backend/src/services/tasks/index.ts
+++ backend/src/services/tasks/index.ts
@@ -3,11 +3,13 @@ import { PostTaskCreate } from "./PostTaskCreate";
 import { DeleteTask } from "./DeleteTask";
 import { PostUpdateTaskDescription } from "./PostUpdateTaskDescription";
 import { PostUpdateTaskDueDate } from "./PostUpdateTaskDueDate";
+import { PostUpdateTask } from "./PostUpdateTask";
 
 export default [
   GetTaskData,
   PostTaskCreate,
   DeleteTask,
   PostUpdateTaskDescription,
-  PostUpdateTaskDueDate
+  PostUpdateTaskDueDate,
+  PostUpdateTask
 ];
